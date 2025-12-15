ec6df7f6e85d73e517ab8d6e733345f55d0f786d
AdityaK0 <aditya123905@gmail.com>
2025-12-15 13:51:00 +0530

    mistake 1

--- db/postgres_reader.py
+++ db/postgres_reader.py
@@ -2,7 +2,7 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from utils import fetch_portfolio_full_json
+from .utils import fetch_portfolio_full_json
 
 
 class PostgresReader:
